bf64b6d1a3817eebd7c6da826de0b62d2de7c805
Navashub <navasmuller01@gmail.com>
2023-10-30 13:39:28 +0300

    assignment done

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,44 @@
 import React from "react";
 
-function Item({ item }) {
+function Item({ item, onUpdateItem, onDeleteItem }) {
+
+  //function to handle button click
+  function handleAddToCartClick() {
+
+    //add fetch request
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        isInCart: !item.isInCart,
+      }),
+    })
+    .then((r) => r.json())
+    .then((updatedItem) => onUpdateItem(updatedItem));
+  }
+
+  function handleDeleteClick() {
+    //our delete request
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "DELETE",
+    })
+    .then((r) => r.json())
+    .then(() => onDeleteItem(item));
+  }
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
       <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
+      {/*adding onCLick listner */}
+      <button
+        className={item.isInCart ? "remove" : "add"}
+        onClick={handleAddToCartClick}>
         {item.isInCart ? "Remove From" : "Add to"} Cart
       </button>
-      <button className="remove">Delete</button>
+      {/*add onCLick */}
+      <button className="remove" onClick={handleDeleteClick}>Delete</button>
     </li>
   );
 }
